f62aeb4ac09dfebfb85cbc5090879b0e02eb7693
Fiqi Andri R <43869030+LITTLEnstein@users.noreply.github.com>
2018-10-17 13:36:02 +0700

    Update AnimalTest.java

--- AnimalTest.java
+++ AnimalTest.java
@@ -2,10 +2,13 @@ public class AnimalTest{
 
 	public static void main(String[] args) {
 		Animal a=new Animal();
+		Cow cow=new Cow();
+		Cat cow=new Cat();
+		
 		a.breath();
 		cat.grow();
 		cat.mencakar();
-		Cow cow=new Cow();
+		cat.breath();
 		cow.breath();
 		cow.grow();
 		cow.menanduk();
